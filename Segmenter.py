9b5017f90ccbf509c79c1988284257ee42262eec
ixlan <bulletdll@gmail.com>
2016-04-14 17:53:58 +0200

    created vocab class + bug fixes

--- Segmenter.py
+++ Segmenter.py
@@ -1,31 +1,38 @@
 import numpy as np
-from lib.Helpers import insert_symbol, remove_symbol, bern, get_words_freq, get_all_words_freq,change_freq, put_boundaries_randomly, get_current_word
+from lib.Helpers import insert_symbol, remove_symbol, bern, get_words_freq, get_all_words_freq, put_boundaries_randomly, get_current_word
+from lib.FreqVocab import FreqVocab
 
 
 class Segmenter:
-    BOUNDARY = '.'  # the boundary symbol
-    RANDOM_BOUND_PROB = 1 # the random boundary probability constant that controls what is a probability of putting initially boundaries in the text
-    TOTAL_SYMBOL = '_TOTAL_'
+    __BOUNDARY = '.'  # the boundary symbol
+    __RANDOM_BOUND_PROB = 0.1 # the random boundary probability constant that controls what is a probability of putting initially boundaries in the text
+    __TOTAL_SYMBOL = '_TOTAL_'
+
 
     # inputs:
     #   p : beta parameter, i.e. number of heads and tails
-    #   alpha : DP hyperparameter proportional to the probability of visitors to sit on an unoccupied table
-    def __init__(self, char_freq, p, alpha):
+    #   alpha : DP hyper-parameter proportional to the probability of visitors to sit on an unoccupied table
+    def __init__(self, text, char_freq, p, alpha, p_dollar=0.2):
         self.char_freq = char_freq
         self.p = p
         self.alpha = alpha
+        self.p_dollar = p_dollar # this hyperparameter is used in P_0
+
+        self.m = np.shape(text)[0] # number of sentences
+        self.text = put_boundaries_randomly(text,self.__RANDOM_BOUND_PROB)
+
+
+        # creating vocab
+        freq = get_all_words_freq(self.text, self.__BOUNDARY)
+        self.word_freq = FreqVocab(freq, self.__TOTAL_SYMBOL)
 
 
     # inputs:
     #   data: the preprocessed array of concatenated string sentences
-    def run(self, text, iter):
-        self.m = np.shape(text)[0] # number of sentences
-        text = put_boundaries_randomly(text,self.RANDOM_BOUND_PROB)
-        self.word_freq = get_all_words_freq(text,self.BOUNDARY)
-
+    def run(self, iter):
         for i in range(iter):
-            text = self.__gibbs(text)
-        print(text)
+            self.text = self.__gibbs(self.text)
+        print(self.text)
 
 
     def __gibbs(self, text):
@@ -37,10 +44,10 @@ class Segmenter:
                 n = len(sent)
                 # since we will change the length of the sentence the stopping condition is dynamic
                 if j == n - 1: break
-                p = 0 #self.__boundary_prob(sent,j)
-                self.__update_word_freq(sent, remove=True)
+                p = 1#self.__boundary_prob(sent,j)
+                self.word_freq.update_freq(sent, remove=True)
                 sent = self.__action(sent, j, p)
-                self.__update_word_freq(sent, remove=False)
+                self.word_freq.update_freq(sent, remove=False)
 
                 # adjust positions based on the decision that has been made
                 if len(sent)>n: # we added a boundary
@@ -60,10 +67,10 @@ class Segmenter:
     #   p: probability for a new boundary
     def __action(self, sent, i, p):
         if bern(p):
-            if not (sent[i+1] == self.BOUNDARY or sent[i] == self.BOUNDARY):  # leave it the way it is if the current position has a boundary symbol
-                sent = insert_symbol(sent, i, self.BOUNDARY)
+            if not (sent[i+1] == self.__BOUNDARY or sent[i] == self.__BOUNDARY):  # leave it the way it is if the current position has a boundary symbol
+                sent = insert_symbol(sent, i, self.__BOUNDARY)
         else:
-            if sent[i] == self.BOUNDARY:
+            if sent[i] == self.__BOUNDARY:
                 sent = remove_symbol(sent, i)
         return sent
 
@@ -83,36 +90,28 @@ class Segmenter:
         cur_word = get_current_word(temp_sent, i)
 
         m = len(sent)
-        n_c_w = self.word_freq[cur_word] if cur_word in self.word_freq else 0 # freq of the current word in the corpus
-        n_w = self.word_freq[self.TOTAL_SYMBOL]
+        n_c_w = self.word_freq.get_freq(cur_word) # freq of the current word in the corpus
+        n_w = self.word_freq.get_total_freq()
         n_u = self.m if get_current_word(temp_sent,m-1) == cur_word else n_w - self.m # here we check if the current word is the utterance final
 
-        return (n_c_w + self.alpha*self.__P0(cur_word))*(n_u + self.p/2)/((n_w + self.alpha)*(n_w + self.p))
+        enum = (n_c_w + self.alpha*self.P0(cur_word))*(n_u + self.p/2)
+        denom =(n_w + self.alpha)*(n_w + self.p)
+        return float(enum)/float(denom)
 
     def __h2(self,sent,i):
         return 0.2
 
 
-    # two stage mechanism, the first stage first subtracts all the words in a sentence
-    # and the second one adds them back when action has taken place
-    # stages are chosen via inserted flag
-    def __update_word_freq(self, sent, remove=False):
-        freq = get_words_freq(sent,self.BOUNDARY)
-        if remove:
-            self.word_freq=change_freq(self.word_freq,freq, remove=True)
-        else:
-            self.word_freq=change_freq(self.word_freq,freq, remove=False)
-
-
 
     # 1. base distribution
-    def __P0(self, w):
+    def P0(self, w):
         m = len(w)
-        res = 1
+        res = 1.0
+        total = float(self.char_freq[self.__TOTAL_SYMBOL])
         for i in range(m):
             char = w[i].lower()
             count = self.char_freq[char] if char in self.char_freq else 0
-            res *= count / self.char_freq[self.TOTAL_SYMBOL]
-        return self.p * (1 - self.p)**(m - 1) * res
+            res *= float(count) / total
+        return res*self.p_dollar * (1 - self.p_dollar)**(m - 1)
 
 
--- run.py
+++ run.py
@@ -8,11 +8,5 @@ from Segmenter import Segmenter
 #file_path = 'data/br-phono-train.txt'
 file_path = 'data/small.txt'
 (text,word_freq,char_freq) = load_text(file_path)
-segmenter = Segmenter(char_freq=char_freq, p=0.5, alpha=0.5)
-segmenter.run(text,10)
-
-text=put_boundaries_randomly(text,0.5)
-sent = text[0]
-print(get_current_word(text[0],len(sent)-1))
-
-print(text)
+segmenter = Segmenter(text,char_freq=char_freq, p=0, alpha=2000)
+segmenter.run(10)
